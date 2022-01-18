3cd78caa5d84928940daa9edce36fa2c27238eb7
songseunghyun <songsh96927@gmail.com>
2022-01-18 16:35:46 +0900

    22.01.18

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,13 @@ class _MyAppState extends State<MyApp> {
       "id": 1,
       "date": 20220117,
       "title": "운동",
-      "text": "6시 재성이랑 운동",
+      "text": "6시 운동",
     },
     {
       "id": 2,
       "date": 20220117,
       "title": "술약속",
-      "text": "8시 당산 술약속",
+      "text": "8시 약속",
     },
   ];
   addData(){
@@ -39,13 +39,33 @@ class _MyAppState extends State<MyApp> {
   }
 
   delData(i){
+
     setState(() {
-      data.remove(data[i]);
+      for(var a = 0 ; a < data.length ; a++) {
+        if (data[a]['id'] == i) {
+          data.remove(data[a]);
+        }
+      };
     });
   }
 
-  updateData(){
-
+  updateData(id, date, title, text){
+    setState(() {
+      for(var a = 0 ; a < data.length ; a++) {
+        if (data[a]['id'] == id) {
+          if(date.text != ""){
+            data[a]['date'] = date.text;
+            print(data);
+          }
+          if(title.text != ""){
+            data[a]['title'] = title.text;
+          }
+          if(text.text != ""){
+            data[a]['text'] = text.text;
+          }
+        }
+      }
+    });
   }
 
   @override
@@ -54,6 +74,14 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text('Scheduler'),
         backgroundColor: Colors.teal,
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add_box_outlined),
+              onPressed: (){
+                //addData();
+              },
+          )
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
@@ -89,7 +117,7 @@ class _MyAppState extends State<MyApp> {
         selectedItemColor: Colors.teal,
       ),
       body: [
-        todaySchedule(data: data , delData : delData),
+        todaySchedule(data: data , delData : delData , updateData: updateData,),
         weekSchedule(),
         memoTodo()
       ][tab],
@@ -98,9 +126,12 @@ class _MyAppState extends State<MyApp> {
 }
 
 class todaySchedule extends StatelessWidget {
-  todaySchedule({Key? key, this.data , this.delData}) : super(key: key);
+  todaySchedule({Key? key, this.data , this.delData, this.updateData}) : super(key: key);
+
   final delData;
+  final updateData;
   final data;
+
   //var count = 10;
   var scroll = ScrollController();
   @override
@@ -128,11 +159,11 @@ class todaySchedule extends StatelessWidget {
             Row(
               children: [
                 IconButton( icon: Icon(Icons.check_box_outlined) ,onPressed:(){
-                  delData(i);
+                  delData(data[i]['id']);
                 }),
                 IconButton( icon: Icon(Icons.settings_applications), onPressed: (){
                   Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => updatePage(data : data[i]))
+                      MaterialPageRoute(builder: (context) => updatePage(data : data[i], id : data[i]['id'] , delData: delData, updateData : updateData,))
                   );
                 },)
               ]
@@ -167,9 +198,15 @@ class memoTodo extends StatelessWidget {
 }
 
 class updatePage extends StatelessWidget {
-  const updatePage({Key? key , this.data}) : super(key: key);
+  updatePage({Key? key , this.data, this.updateData, this.delData , this.id}) : super(key: key);
 
   final data;
+  final updateData;
+  final delData;
+  final id;
+  var inputDate = TextEditingController();
+  var inputTitle = TextEditingController();
+  var inputText = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -179,24 +216,109 @@ class updatePage extends StatelessWidget {
           backgroundColor: Colors.teal,
         ),
         body: Container(
-            child: Text('Update Page')
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('날짜'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: inputDate,
+                    decoration: InputDecoration(
+                      hintText: data['date'].toString(),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.blue),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.teal),
+                      ),
+                    ),
+                  ),
+                ),
 
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.check),
-              label: 'save',
+                Text('제목'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: inputTitle,
+                    decoration: InputDecoration(
+                      hintText: '${data['title']}',
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.blue),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.teal),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Text('내용'),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: inputText,
+                    decoration: InputDecoration(
+                      hintText: data['text'],
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.blue),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(width: 1, color: Colors.teal),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4)
+                        )
+                      ) ,
+                      child: IconButton(
+                          onPressed: (){
+                            updateData(id,inputDate,inputTitle,inputText);
+                            print(inputDate);
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(Icons.check)
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(4)
+                          )
+                      ) ,
+                      child: IconButton(
+                          onPressed: (){
+                            delData(data['id']);
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(Icons.delete_outline_sharp)
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
+        ),
 
-            BottomNavigationBarItem(
-              icon: Icon(Icons.delete_outline_sharp),
-              label: 'delete',
-            )
-          ],
-        )
     );
   }
 }
