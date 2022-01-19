c74f8daaef0217224b4bcea1db210de8a8429175
songseunghyun <songsh96927@gmail.com>
2022-01-19 14:17:39 +0900

    22.01.19

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -30,8 +31,19 @@ class _MyAppState extends State<MyApp> {
       "text": "8시 약속",
     },
   ];
-  addData(){
 
+  addData(date, title, text){
+    DateTime now = DateTime.now();
+    var newData = {
+      "id": DateFormat.yMd().add_jms().format(now),
+      "date": date.toString(),
+      "title": title.toString(),
+      "text": text.toString(),
+    };
+    setState(() {
+      data.add(newData);
+      print(newData['id']);
+    });
   }
 
   getData(){
@@ -39,7 +51,6 @@ class _MyAppState extends State<MyApp> {
   }
 
   delData(i){
-
     setState(() {
       for(var a = 0 ; a < data.length ; a++) {
         if (data[a]['id'] == i) {
@@ -78,6 +89,9 @@ class _MyAppState extends State<MyApp> {
           IconButton(
               icon: Icon(Icons.add_box_outlined),
               onPressed: (){
+                showDialog(context: context, builder: (context){
+                  return addSchedule(addData : addData);
+                });
                 //addData();
               },
           )
@@ -181,7 +195,7 @@ class weekSchedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('주간 스케쥴'),
+      child: Text('week schedule'),
     );
   }
 }
@@ -323,3 +337,95 @@ class updatePage extends StatelessWidget {
   }
 }
 
+class addSchedule extends StatelessWidget {
+  addSchedule({Key? key, this.addData}) : super(key: key);
+
+  final addData;
+  var inputDate = TextEditingController();
+  var inputTitle = TextEditingController();
+  var inputText = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        width: 400,
+        height: 330,
+        child: Column(
+          children: [
+            Text('일정 추가', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: inputDate,
+                decoration: InputDecoration(
+                  hintText: 'Date',
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(width: 1, color: Colors.blue),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(width: 1, color: Colors.teal),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: inputTitle,
+                decoration: InputDecoration(
+                  hintText: 'Title',
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(width: 1, color: Colors.blue),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(width: 1, color: Colors.teal),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: inputText,
+                decoration: InputDecoration(
+                  hintText: 'Text',
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(width: 1, color: Colors.blue),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(width: 1, color: Colors.teal),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.check),
+                  onPressed: (){
+                    addData(inputDate.text, inputTitle.text, inputText.text);
+                    Navigator.pop(context);
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.cancel_outlined),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
