bc768693244b5d5e0fecec6f5c492ad40ef335ca
songseunghyun <songsh96927@gmail.com>
2022-01-28 23:24:11 +0900

    22.01.28

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var tab = 0;
-  var data = [
-
-  ];
+  var data = [];
 
   @override
   void initState(){
@@ -35,7 +33,7 @@ class _MyAppState extends State<MyApp> {
   addData(date, title, text) async{
     DateTime now = DateTime.now();
     var newData = {
-      "id": DateFormat.yMd().add_jms().format(now),
+      "id": DateFormat('yyyy.MM.dd.HH.mm.ss').format(now),
       "date": date.toString(),
       "title": title.toString(),
       "text": text.toString(),
@@ -43,10 +41,14 @@ class _MyAppState extends State<MyApp> {
     http.Response res = await http.post(
         Uri.parse('http://localhost:3000/scheduler'),
         headers: {"Content-Type" : "application/json"},
-        body:jsonEncode(newData));
+        body:jsonEncode(newData)
+    );
+    print(newData['id']);
     setState(() {
       data.add(newData);
     });
+
+
   }
 
   getData() async{
@@ -54,13 +56,17 @@ class _MyAppState extends State<MyApp> {
     data = jsonDecode(getServerData.body);
   }
 
-  delData(i){
-    setState(() {
+  delData(id) async{
+
+    http.Response res = await http.delete(
+      Uri.parse('http://localhost:3000/scheduler/${id}'),
+    );
+    setState(() {//TODO foreach로 바꾸기
       for(var a = 0 ; a < data.length ; a++) {
-        if (data[a]['id'] == i) {
+        if (data[a]['id'] == id) {
           data.remove(data[a]);
         }
-      };
+      }
     });
   }
 
@@ -68,15 +74,9 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       for(var a = 0 ; a < data.length ; a++) {
         if (data[a]['id'] == id) {
-          if(date.text != ""){
-            data[a]['date'] = date.text;
-          }
-          if(title.text != ""){
-            data[a]['title'] = title.text;
-          }
-          if(text.text != ""){
-            data[a]['text'] = text.text;
-          }
+          if(date.text != ""){ data[a]['date'] = date.text; }
+          if(title.text != ""){ data[a]['title'] = title.text; }
+          if(text.text != ""){ data[a]['text'] = text.text; }
         }
       }
     });
@@ -144,53 +144,88 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class todaySchedule extends StatelessWidget {
+class todaySchedule extends StatefulWidget {
   todaySchedule({Key? key, this.data , this.delData, this.updateData}) : super(key: key);
 
   final delData;
   final updateData;
   final data;
 
+  @override
+  State<todaySchedule> createState() => _todayScheduleState();
+}
+
+class _todayScheduleState extends State<todaySchedule> {
+  bool _isChecked = false;
+
+
   //var count = 10;
   var scroll = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: data.length ,controller: scroll ,itemBuilder: (c,i){
-      return Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          borderRadius: BorderRadius.all(Radius.circular(4.0))
+    return Column(
+      children: [
+        SizedBox(
+          child: Row(
+            children: [
+              Text('오늘일정만 보기'),
+              Switch(
+                value: _isChecked,
+                onChanged: (value){
+                  setState(() {
+                    _isChecked = value;
+                    print(widget.data);
+                  });
+                },
+              ),
+            ],
+          ),
         ),
-        padding: EdgeInsets.all(10),
-        margin: EdgeInsets.fromLTRB(2, 5, 2, 5),
-        alignment: Alignment.topLeft,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('시간: ${data[i]['date'].toString()}'),
-                Text('내용: ${data[i]['title']}'),
-                Text('상세내용: ${data[i]['text']}')
-              ],
-            ),
-            Row(
-              children: [
-                IconButton( icon: Icon(Icons.check_box_outlined) ,onPressed:(){
-                  delData(data[i]['id']);
-                }),
-                IconButton( icon: Icon(Icons.settings_applications), onPressed: (){
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => updatePage(data : data[i], id : data[i]['id'] , delData: delData, updateData : updateData,))
-                  );
-                },)
-              ]
-            )
-          ],
-        ),
-      );
-    });
+        Flexible(
+          child: ListView.builder(itemCount: widget.data.length ,controller: scroll ,itemBuilder: (c,i){
+            return Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(color: Colors.grey, offset: Offset(0.0,1.0),blurRadius: 6.0)
+                ]
+              ),
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.fromLTRB(2, 5, 2, 5),
+              alignment: Alignment.topLeft,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('시간: ${widget.data[i]['date'].toString()}'),
+                      Text('내용: ${widget.data[i]['title']}'),
+                      Text('상세내용: ${widget.data[i]['text']}')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      IconButton( icon: Icon(Icons.check_box_outlined) ,onPressed:(){
+                        widget.delData(widget.data[i]['id']);
+                      }),
+                      IconButton( icon: Icon(Icons.settings_applications), onPressed: (){
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => updatePage(data : widget.data[i], id : widget.data[i]['id'] , delData: widget.delData, updateData : widget.updateData,))
+                        );
+                      },)
+                    ]
+                  )
+                ],
+              ),
+            );
+          }),
+        )
+      ],
+    );
   }
 }
 
